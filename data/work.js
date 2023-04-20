8b9e9e52f1903b561bd152215edee90460ab606f
Tim Carew <mail@timcarew.com>
2023-04-20 20:09:30 -0230

    update copy

---
+++ data/work.js
@@ -0,0 +1,52 @@
+export const work = [
+  {
+    title: 'NL MindSet',
+    href: 'https://nlmindset.ca',
+    media:
+      'https://the-message.ca/2023/03/20/newfoundlands-mindset-is-in-the-cards/',
+    description:
+      'The Newfoundland & Labrador MindSet is a collection of mindfulness cards that opens a window to the Newfoundland and Labrador way of life, and encourages making the most of every moment.',
+  },
+  {
+    title: 'Come Home 2022',
+    href: 'https://archive.comehome2022.ca/ecardbuilder',
+    media:
+      'https://awards.strategyonline.ca/winners/winner/2022/?e=141148&n=Come+Home+2022',
+    description:
+      'Come Home 2022 encouraged former residents of Newfoundland and Labrador now living away to come home, and reminded residents of the province of the wonders here in their own ‘backyard’.',
+  },
+  {
+    title: "Don't Get Diepped (Stallmark)",
+    href: 'https://dontgetdiepped.ca',
+    media:
+      'https://the-message.ca/2022/11/28/these-cards-apologize-to-newfoundlanders-whose-gift-got-diepped/',
+    description:
+      'The Stallmark collection is an assortment of greeting cards that helps gift givers not show up empty handed when their gift is stuck in the mail, while reminding them (in funny, NL fashion) to shop locally.',
+  },
+  {
+    title: 'Story Exchange',
+    href: 'https://www.storyexchange.com/',
+    description:
+      'The Story Exchange is a canvas for Newfoundland and Labrador travel stories. A place to recount most memorable adventures, showcase photos, and share some words of wisdom with future travellers to the province. All in the form of a shareable digital book.',
+  },
+  {
+    title: 'Anything But Ordinary',
+    href: 'https://anythingbutordinary.travel/',
+    media:
+      'https://www.theglobeandmail.com/life/adv/article-a-virtual-trip-to-a-most-unique-destination/',
+    description:
+      "A place to enjoy travel articles, videos, and all things Newfoundland and Labrador at NL Tourism' content hub: Anything but Ordinary.",
+  },
+  {
+    title: 'Choose Small (Loyalist College)',
+    href: 'https://choosesmall.ca/',
+    media:
+      'https://loyalistcollege.com/news/loyalist-college-invites-community-to-choose-small-for-launch-of-new-brand-identity/',
+    description: '',
+  },
+  {
+    title: 'No Picnik',
+    href: 'https://nopicnik.ca/',
+    description: '',
+  },
+];
